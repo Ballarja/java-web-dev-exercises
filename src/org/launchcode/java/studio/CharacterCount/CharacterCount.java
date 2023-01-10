b9b301238acd07fb6b5fab2f1871443aabced51b
Ballarja <JamesBallard87@hotmail.com>
2023-01-09 20:02:25 -0600

    Commit for studio

---
+++ src/org/launchcode/java/studio/CharacterCount/CharacterCount.java
@@ -0,0 +1,56 @@
+package org.launchcode.java.studio.CharacterCount;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class CharacterCount {
+
+    public static void main(String[] args){
+
+        String text = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+
+        char[] characters = text.toCharArray();
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<Character> foundCharacters = new ArrayList<>();
+       HashMap<Character, Integer> characterCounts = new HashMap<>();
+
+        System.out.println("Enter a Line count");
+        String input = scan.nextLine();
+
+        if (!input.equals("")) {
+            characters = input.toCharArray();
+        }
+
+
+        for (int i = 0; i < characters.length; i++) {
+
+            Character c = Character.toLowerCase(characters[i]);
+
+            if (c.equals(' ') || c.equals(',') || c.equals('.') || c.equals('’')) {
+                continue;
+            }
+
+            // init list of characters and count
+            if (!foundCharacters.contains(c)) {
+                foundCharacters.add(c);
+                characterCounts.put(c, 0);
+            }
+
+            // count found characters
+            characterCounts.put(c, characterCounts.get(c) + 1);
+
+        }
+
+        Collections.sort(foundCharacters);
+
+        for (Character c : foundCharacters) {
+
+            System.out.println(c + ": " + characterCounts.get(c));
+
+        }
+
+    }
+}
